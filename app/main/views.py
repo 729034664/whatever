5402add03d778636bca69f826477c17c9e769dcc
zhang <729034664@qq.com>
2018-11-08 22:20:47 +0800

    11.3未调试

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template,session,redirect,url_for, abort, flash
+from flask import render_template,session,redirect,url_for, abort, flash, request, current_app
 from flask_login import login_required, current_user
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from ..decorators import admin_required
@@ -20,8 +20,15 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc().paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False))
+
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts,
+                           pagination=pagination)
 
 
 @main.route('/user/<username>')
